4a00f1a4946dbd32f00c5a766ead64bc2c974ad5
nbertagnolli <nmbertagnolli@gmail.com>
2019-10-05 12:42:48 -0600

    Added setup script

---
+++ setup_new_max.sh
@@ -0,0 +1,45 @@
+# unhide all files
+defaults write com.apple.finder AppleShowAllFiles true
+killall Finder
+
+# Change the name configuration in terminal
+echo "export PS1='\W \$ '" >> ~/.bash_profile
+source .bash_profile
+
+# Make a repositories directory
+mkdir repos
+
+# generate an ssh key
+ssh-keygen -t rsa
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install python
+brew install python
+
+# Install Bazel
+brew tap bazelbuild/tap
+brew tap-pin bazelbuild/tap
+brew install bazel
+
+# Install Java stuff
+brew cask install java
+brew install tomcat
+brew install maven
+
+# install postgres
+brew install postgresql
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+alias pg_stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+
+
+# Install python requirements
+pip3 install -r requirements.txt
+
+# Install ruby gems
+gem install --user sass
+
+
+
