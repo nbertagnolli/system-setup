6b0d26d027c87811f7687f4a7e9f21caff5727dd
nicolas bertagnolli <tetracycline@nicolass-MacBook-Pro-2.local>
2022-06-19 20:04:33 -0700

    Updated installs

--- setup_new_max.sh
+++ setup_new_max.sh
@@ -2,20 +2,25 @@
 defaults write com.apple.finder AppleShowAllFiles true
 killall Finder
 
-# Change the name configuration in terminal
-echo "export PS1='\W \$ '" >> ~/.bash_profile
-echo "export PS1='\W \$ '" >> ~/.zshrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
-source ~/.bash_profile
+# Install xcode
+xcode-select --install
 
 # Make a repositories directory
 mkdir repos
 
 # generate an ssh key
-# ssh-keygen -t rsa
+ssh-keygen -t rsa
+
+# Change the name configuration in terminal
+echo "export PS1='> '" >> ~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+source ~/.zshrc
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
 
 # Install random brew libraries
 brew install wget
@@ -33,13 +38,10 @@ brew install poppler
 # Install python
 brew install python
 brew install pyenv
-pyenv install 3.7.7
-pyenv global 3.7.3
-
-# Install Bazel
-brew tap bazelbuild/tap
-brew tap-pin bazelbuild/tap
-brew install bazel
+brew install pyenv-virtualenv
+brew upgrade pyenv
+pyenv install 3.10.4
+pyenv global 3.10.4
 
 # Install Java stuff
 brew cask install java
@@ -60,11 +62,6 @@ brew install gnupg
 python3 -m pip install --upgrade pip --user
 python3 -m pip install -r requirements.txt --user
 python3 -m spacy download en_core_web_sm
-
-# Install non system version of ruby
-# brew install ruby
-# echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.bash_profile
-# source ~/.bash_profile
 
 # Install ruby gems
 gem install --user sass
